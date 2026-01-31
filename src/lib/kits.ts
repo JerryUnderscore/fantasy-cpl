302c1587ea12f2d083fcbb5c4a34764f1b22dd93
Jerry Wurnig <wurnig@gmail.com>
2026-01-30 20:56:51 -0400

    Update: 2026 kits for Atletico, Forge, Supra & Vancouver

--- src/lib/kits.ts
+++ src/lib/kits.ts
@@ -1,5 +1,8 @@
 export const getKitSrc = (slug?: string | null) => {
   if (!slug) return null;
+  if (slug === "atletico-ottawa") return "/kits/atletico-ottawa.png";
+  if (slug === "forge") return "/kits/forge.png";
+  if (slug === "vancouver") return "/kits/vancouver.png";
   if (slug === "supra") return "/kits/supra.png";
   return `/kits/${slug}.svg`;
 };
